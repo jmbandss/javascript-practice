16ff29e34f4d92112d822e460592aad045069784
jmbandss <miguel.ramirez@stu.vertexacademies.org>
2024-10-15 14:04:43 -0400

    Add files via upload

---
+++ Miguel_Ramirez_JS_00 (1).js
@@ -0,0 +1,13 @@
+
+
+
+
+//Problem #2
+Console.log(" Hola,Mundo!")
+
+// Problem #3
+Console.log("Hello,")
+console.log("World!")
+
+// Problem #4
+console.log("The computer says, \"Hello World\"");
