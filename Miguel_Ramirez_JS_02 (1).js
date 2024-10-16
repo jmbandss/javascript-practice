36ccc6eb4ce37f4ffc808f1b6f1ee62ce3852c0c
jmbandss <miguel.ramirez@stu.vertexacademies.org>
2024-10-16 14:09:11 -0400

    Add files via upload

---
+++ Miguel_Ramirez_JS_02 (1).js
@@ -0,0 +1,16 @@
+// Example:             // Expected Output
+console.log(4 + 2.5)   // 6.5
+
+// Write 5 more expressions below:
+console.log(7+3.25)   // 10.25
+console.log(9*4.5)   // 40.5
+console.log(43-34.9) // 8.100000000000001
+console.log(48/6)    // 8
+console.log(6*32)    //192
+//Boolean work
+console.log(!true);
+console.log(!!false);
+console.log(true||true);
+console.log(false||true);
+console.log(true || true); 
+console.log(false || true); 
