c73b1f3e3685fc67002f9ffbc90ad8a1741dbf23
jmbandss <miguel.ramirez@stu.vertexacademies.org>
2024-10-18 13:52:11 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+// 1. Declare a variable called `firstName` and assign it your first name as a string:
+
+
+// 2. Declare a variable called `lastName` and assign it your last name as a string:
+
+
+// 3. Declare a variable called `age`:
+
+
+// 4. Print out the `firstName`, `lastName`, and `age` variables. What do you
+//    expect them to be when they get printed?
+
+
+// 5. Assign the `age` variable to a number:
+
+
+// 6. Print out the `firstName`, `lastName`, and `age` variables. What do you
+//    expect them to be when they get printed?
+
+let Fname = "Miguel";
+let Lname = "Ramirez";
+console.log("Full Name:", Fname +" "+Lname)
+let age = "17"
+console.log("age:", age)
+let Birth = "2007"
+console.log(2024-17)
